264fcfcea389c699cdc25b0ef688b11b7225e3a8
Uzoamaka126 <uzoamakaanyanwu26@gmail.com>
2024-02-03 09:00:13 +0100

    cleanup: remove unused files

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,42 @@
 <template>
   <div>
-    <BaseUpload>
-      <template #triggerButton>Click here to upload</template>
-    </BaseUpload>
+    <component :is="props.buttonTag" @click="openModal" :class="props.buttonClass">
+        <slot name="triggerButton" />
+    </component>
+    <div>
+        <UploadModal v-bind="props" v-model:show="isModalOpen" v-model:files="fileList" />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
-// import { AppProps } from './components/types.ts';
-import BaseUpload from './components/BaseUpload.vue'
+import UploadModal from './components/UploadModal.vue'
+import { AppProps } from './components/types';
 
+// import { AppProps } from './components/types.ts';
+// import BaseUpload from './components/BaseUpload.vue'
+const props = withDefaults(defineProps<Partial<AppProps>>(), {
+  buttonTag: 'button',
+  buttonClass: 'btn btn--text sm',
+  isMulti: true,
+  mimeTypes: ".pdf",
+  maxUploadSize: 5000 * 1000, // 5MB
+  styles: () => ({}),
+  persist: true,
+  align: 'center',
+  disabled: false,
+  modalContentClasses: '',
+  files: null
+});
+
+  const fileList = ref([] as any[]);
+
+  const isModalOpen = ref(false);
+
+  const openModal = () => {  
+    isModalOpen.value = true;
+  }
 </script>
 
 <style scoped>
--- src/index.ts
+++
@@ -1,3 +0,0 @@
-// lib/main.js export all components
-import BaseUpload from './components/BaseUpload.vue'
-export { BaseUpload }
